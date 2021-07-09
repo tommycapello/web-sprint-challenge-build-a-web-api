0fd596a7a58a0b0520dce15bdfdfe70c17e8cf24
tommycapello <thomcapello@gmail.com>
2021-07-09 10:32:43 -0400

    initialized project

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,4 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model');
+const router = express.Router();
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,4 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+const router = express.Router();
