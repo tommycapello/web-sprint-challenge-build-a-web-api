beea8ec83caf9e194111d52fddf42d1cdfa8afda
tommycapello <thomcapello@gmail.com>
2021-07-09 11:35:13 -0400

    9 tests passing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,3 +2,53 @@
 const express = require('express');
 const Projects = require('./projects-model');
 const router = express.Router();
+
+router.get('/', async(req,res) => {
+    try{
+        const projects = await Projects.get()
+        if(projects.length === 0){
+            res.status(200).json([])
+        }
+        else(
+            res.status(200).json(projects)
+        )
+    }
+    catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+router.get('/:id', async (req,res) =>{
+    if(!req.params.id){
+        res.status(404).json({
+            message: 'There is no project with the given ID'
+        })
+    }
+    else{
+        const project = await Projects.get(req.params.id);
+        res.json(project)
+    }
+})
+
+router.post('/', (req,res) =>{
+    const body = req.body
+    if(!body){
+        res.status(400).json({
+            message: 'Please complete all the required fields'
+        })
+    }
+    else{Projects.insert(req.body)
+        .then(project => res.json(project))
+        .catch(err => {
+        res.status(500).json({
+        message: err.message})
+        })
+        }
+    })
+
+
+    
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 // Configure your server here
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
 
 server.use(express.json());
 
 // Build your actions router in /api/actions/actions-router.js
+
 // Build your projects router in /api/projects/projects-router.js
+server.use('/api/projects', projectsRouter)
+
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
